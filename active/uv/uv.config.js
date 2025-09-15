8725b542217b149792f94ad4a7e408c7c488df51
Your Name <you@example.com>
2025-09-14 22:54:53 -0500

    u

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,10 +1,16 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare: 'https://math.cinevez.lol/bare/',
+    bare: 'https://epoxy.barndog.pro',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
     bundle: '/active/uv/uv.bundle.js',
     config: '/active/uv/uv.config.js',
     sw: '/active/uv/uv.sw.js',
+    transport: {
+        type: 'epoxy',
+        config: {
+            path: '/epoxy'
+        }
+    }
 };
